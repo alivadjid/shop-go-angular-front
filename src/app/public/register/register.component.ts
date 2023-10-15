6be1d7d4687725a93b056cead28af461e937cf79
alivadjid <snub01@mail.ru>
2023-10-15 22:55:05 +0300

    send register to api

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,16 +6,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   firsName = '';
   lastName = '';
   email = '';
   password = '';
   passwordConfirm = '';
 
-  // ngOnInit(): void {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
 
   submit() {
-    console.log('confirm', this.firsName);
+    this.http
+      .post('http://localhost:8000/api/register', {
+        first_name: this.firsName,
+        last_name: this.lastName,
+        email: this.email,
+        password: this.password,
+        password_confirm: this.passwordConfirm,
+      })
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
